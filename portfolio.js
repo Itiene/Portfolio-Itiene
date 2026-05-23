ea91a6bd4bf313ea437f8f2c321a2cbd940aed56
Itiene Guilherme <64323258+Itiene@users.noreply.github.com>
2026-05-23 18:30:07 +0100

    Atualizando

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,28 @@
-const progresso=document.querySelectorAll("progresso")
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
-progresso.addEventListener("onload",(evt)=>{
-    progresso.classList.toggle(".progresso")
-})
+gsap.registerPlugin(ScrollTrigger);
+
+
+const fnome=document.getElementById("fnome");
+const femail=document.getElementById("femail");
+const fmessage=document.getElementById("fmessage");
+const btn=document.getElementById("btn");
+
+
+
+btn.addEventListener("click", function(event){
+    event.preventDefault(); 
+    const nome = fnome.value;
+    const email = femail.value;
+    const message = fmessage.value;
+    if(nome && email && message != ""){
+        alert("Mensagem enviada com sucesso!");
+        fnome.value = nome;
+        femail.value = email;
+        fmessage.value = message;
+    } else {
+        alert("Por favor, preencha todos os campos.");
+    }
+
+})   
